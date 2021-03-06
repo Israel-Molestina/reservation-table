dc0c8e7acdbe25241bf52d2618a95e548b5b8ba8
you know who <chorvickprocode@gmail.com>
2021-03-06 11:06:48 -0500

    updated

--- server.js
+++ server.js
@@ -1,18 +1,34 @@
 // requireing dependencies express + path
+const express = require('express');
+const path = require('path');
 
 // set up express w/ two constants one for app and one for port
 
+const app = express();
+const PORT = 3000;
+
+
+
 // set up express to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //set up route to vew page
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'home.html')));
 
-//set up route to reservation page
 
-// set up route to tables page
 
-// display current reservations
+//set up route to reservation page  xxx
+app.get('/reserve', (req, res) => res.sendFile(path.join(__dirname, 'reserve.html')));
+// set up route to tables pagexxxxx
+app.get('/tables', (req, res) => res.sendFile(path.join(__dirname, 'tables.html')));
+// display current reservations  
 
-// display wait list
+// display wait list  
+app.get('/api/tables', (req, res) => res.json(tobenamed));
+
+app.get('/api/waitlist', (req, res) => res.json(tobenamed2));
+
 
 // create post method to take in new reservation
 
