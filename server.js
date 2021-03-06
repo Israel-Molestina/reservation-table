1a6654c0a91bd4b7bd884e6e1a088a59a560de44
Israel Molestina <molestinaisrael@gmail.com>
2021-03-06 11:28:09 -0500

    removed unncesasary code from post request

--- server.js
+++ server.js
@@ -37,14 +37,9 @@ app.post('/api/tables', (req, res) => {
     // This works because of our body parsing middleware
     const newTable = req.body;
   
-    // Using a RegEx Pattern to remove spaces from newCharacter
-    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    newTable.routeName = newTable.name.replace(/\s+/g, '').toLowerCase();
-    console.log(newTable);
-  
     tables.push(newTable);
     res.json(newTable);
-  });
+});
 
 // start server to begin listening
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
